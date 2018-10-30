0282eb0a5a41522107359a1c23dc41f0267da058
Arvind Thangamani <dnivra26@gmail.com>
2018-10-30 12:20:55 +0600

    couple of services with envoy as front-proxy

---
+++ service_a/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Calling Service B")
+	resp, err := http.Get("http://service_b:8082/")
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	fmt.Fprintf(w, "Hello from service A")
+
+	fmt.Fprintf(w, string(body))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
