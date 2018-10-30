fd2b41aeaad82731df96ca437ff6a18ca3f2f6b5
Arvind Thangamani <dnivra26@gmail.com>
2018-10-30 13:39:23 +0600

    pass tracking headers from service a to service b, whole picture should be available now

--- service_a/main.go
+++ service_a/main.go
@@ -11,7 +11,22 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "Calling Service B")
-	resp, err := http.Get("http://service_a_envoy:8788/")
+
+	req, err := http.NewRequest("GET", "http://service_a_envoy:8788/", nil)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	req.Header.Add("x-request-id", r.Header.Get("x-request-id"))
+	req.Header.Add("x-b3-traceid", r.Header.Get("x-b3-traceid"))
+	req.Header.Add("x-b3-spanid", r.Header.Get("x-b3-spanid"))
+	req.Header.Add("x-b3-parentspanid", r.Header.Get("x-b3-parentspanid"))
+	req.Header.Add("x-b3-sampled", r.Header.Get("x-b3-sampled"))
+	req.Header.Add("x-b3-flags", r.Header.Get("x-b3-flags"))
+	req.Header.Add("x-ot-span-context", r.Header.Get("x-ot-span-context"))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
 
 	if err != nil {
 		fmt.Printf("%s", err)
