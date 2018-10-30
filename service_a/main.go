4912b8a8bb90e4f1b49048d697b6aa470e61b98e
Arvind Thangamani <dnivra26@gmail.com>
2018-10-30 15:20:33 +0600

    add one more service; to have multiple children in a span

--- service_a/main.go
+++ service_a/main.go
@@ -37,7 +37,34 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Printf("%s", err)
 	}
+	fmt.Fprintf(w, string(body))
 	fmt.Fprintf(w, "Hello from service A")
+
+	req, err = http.NewRequest("GET", "http://service_a_envoy:8791/", nil)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	req.Header.Add("x-request-id", r.Header.Get("x-request-id"))
+	req.Header.Add("x-b3-traceid", r.Header.Get("x-b3-traceid"))
+	req.Header.Add("x-b3-spanid", r.Header.Get("x-b3-spanid"))
+	req.Header.Add("x-b3-parentspanid", r.Header.Get("x-b3-parentspanid"))
+	req.Header.Add("x-b3-sampled", r.Header.Get("x-b3-sampled"))
+	req.Header.Add("x-b3-flags", r.Header.Get("x-b3-flags"))
+	req.Header.Add("x-ot-span-context", r.Header.Get("x-ot-span-context"))
+
+	client = &http.Client{}
+	resp, err = client.Do(req)
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
 
 	fmt.Fprintf(w, string(body))
 }
